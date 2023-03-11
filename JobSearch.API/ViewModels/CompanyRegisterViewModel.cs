86b58fa3d1b431a7a09272928e62083049206f0f
Колосс Матвій <kolossmatveyka@gmail.com>
2023-03-11 21:29:44 +0200

    add validtion

--- JobSearch.API/ViewModels/CompanyRegisterViewModel.cs
+++ JobSearch.API/ViewModels/CompanyRegisterViewModel.cs
@@ -5,11 +5,17 @@ namespace JobSearch.API.ViewModels
 {
     public class CompanyRegisterViewModel
     {
+        [Required(ErrorMessage = "Should be not required")]
+        [EmailAddress(ErrorMessage = "It doesn't look like an email")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Should be not required")]
 
         public string Password { get; set; }
+        [Required(ErrorMessage = "Should be not required")]
+        [StringLength(30, MinimumLength = 5, ErrorMessage = "Field should contains from 5 to 30 symbols")]
 
         public string CompanyName { get; set; }
+        [Required(ErrorMessage = "Should be not required")]
 
         public string Address { get; set; }
 
